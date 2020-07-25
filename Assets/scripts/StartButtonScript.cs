f07770e17201179acc97d6509f8f322728cc7451
Marcus <fbisurveillancevan525178@gmail.com>
2020-07-24 20:58:11 -0700

    minor fixes

---
+++ Assets/scripts/StartButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartButtonScript : MonoBehaviour
+{
+    private bool mouseIn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1") && mouseIn)
+        {
+            Debug.Log("Loading Scene");
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        }
+    }
+    void OnMouseEnter()
+    {
+        mouseIn = true;
+    }
+    void OnMouseExit()
+    {
+        mouseIn = false;
+    }
+}
